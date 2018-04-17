d93278fe79415b6fce398e7f586ad0108323afbd
Ethan Nelson <ethannels@gmail.com>
2018-04-16 19:26:53 -0500

    changed port call

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
   res.render('index', {poemTitle: null, poemAuthor: null, poemLines: null, error: null});
@@ -32,6 +32,6 @@ app.post('/', function (req, res) {
 	    }
 	  });
 })
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
 });
